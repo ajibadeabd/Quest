42450fe439719008637497ac73989b23611a87e9
Phyf3 <phade075@gmail.com>
2022-03-31 13:37:28 +0100

    almost done

--- packages/hardhat/deploy/01_deploy_staker.js
+++ packages/hardhat/deploy/01_deploy_staker.js
@@ -1,6 +1,6 @@
 // deploy/01_deploy_staker.js
 
-// const { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deploy } = deployments;
@@ -29,11 +29,11 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   // const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
 
   // If you want to send value to an address from the deployer
-  // const deployerWallet = ethers.provider.getSigner()
-  // await deployerWallet.sendTransaction({
-  //   to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-  //   value: ethers.utils.parseEther("0.001")
-  // })
+  const deployerWallet = ethers.provider.getSigner();
+  await deployerWallet.sendTransaction({
+    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
+    value: ethers.utils.parseEther("0.001"),
+  });
 
   // If you want to send some ETH to a contract on deploy (make your constructor payable!)
   // const yourContract = await deploy("YourContract", [], {
